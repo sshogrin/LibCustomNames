342e9c31e28242685a0bdadd0e28b737fb6c9fe0
m00nyONE <severin.meier.x@gmail.com>
2025-07-27 22:02:30 +0200

    add/edit/remove users

--- names/misc7.lua
+++ names/misc7.lua
@@ -188,3 +188,4 @@ n["@Chay'Kashet"] = {"Chay'Kashet", "|cffe4d9C|r|cffd9c5h|r|cffcfb2a|r|cffc49ey|
 n["@InsaneShad0w92"] = {"Shad0w92", "|c6f32ecS|r|c8339d8h|r|c983fc5a|r|cad46b2d|r|cc14c9e0|r|cd6528bw|r|cea59779|r|cff5f642|r"}
 n["@CRaull"] = {"CRaull", "|cffffffCRaull|r"}
 n["@Manitaropitas"] = {"Mani", "|cbd00ffMani|r"}
+n["@kiyothewanderer"] = {"KIYO", "|cfb8c94K|r|cfaf366I|r|c5eccdaY|r|cc289faO|r"}
