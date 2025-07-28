d3b5d3f8775976cfbcd294e9c706e646fa7c7f8e
Flamebuckler <flamebuckler@gmail.com>
2025-07-28 19:27:39 +0200

    Add custom name for @Minasgorth in misc7.lua

--- names/misc7.lua
+++ names/misc7.lua
@@ -191,4 +191,4 @@ n["@Manitaropitas"] = {"Mani", "|cbd00ffMani|r"}
 n["@kiyothewanderer"] = {"KIYO", "|cfb8c94K|r|cfaf366I|r|c5eccdaY|r|cc289faO|r"}
 n["@IgnisCore"] = {"IgnisCore", "|c1cc50cI|r|c39c917g|r|c55cd22n|r|c71d12di|r|c8ed437s|r|caad842C|r|cc6dc4do|r|ce3e058r|r|cffe463e|r"}
 n["@KendrasKenpachi"] = {"Kendras", "|c960000Kendras|r"}
-
+n["@Minasgorth"] = {"Minas", "|ce4e4e4M|r|cc8c8c8i|r|cadadadn|r|c919191a|r|c767676s|r"}
