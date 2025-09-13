9a29263436157b103dd1e0011984c1a28cefbaf4
sshogrin <125930098+sshogrin@users.noreply.github.com>
2025-09-12 22:21:46 -0500

    Update sshogrin.lua

--- names/sshogrin.lua
+++ names/sshogrin.lua
@@ -53,4 +53,4 @@ n["@SquirrelyWrath"] = {"SquirrelyWrath", "|cee7512S|r|cde7a24q|r|ccd8037u|r|cbc
 n["@djeRNATC"] = {"Daniel (DJ)", "|c467700D|r|c567f00a|r|c678800n|r|c789000i|r|c899900e|r|c99a100l|r |caaaa00(|r|cbbb200D|r|ccbbb00J|r|cdcc300)|r"}
 n["@Evildeadgirl"] = {"Lokia", "|c6b0000Lokia|r"}
 n["@Yosemite_SLAM"] = {"Gladwyn", "|c0024ffG|r|c0049ffl|r|c006dffa|r|c0092fed|r|c00b6few|r|c00dbfey|r|c00fffen|r"}
-
+n["@Brito_68"] = {"Brito", "|ce700ffBrito|r"}
